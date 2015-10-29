b973676136e9e434df5add872727a3c34a7f2f95
rlfs@cin.ufpe.br <salqop21>
2015-10-29 01:38:42 -0300

    aloha - Adicionado a media das 1000 interações.

--- aloha.py
+++ aloha.py
@@ -5,34 +5,38 @@ def aloha(func):
     rand = lambda x: randint(0,x)
     results = []
     for numTags in [(x+1)*100 for x in range(10)]:
-        numSlots = 64
-        result = Result(numTags)
-        while(numTags != 0):
-            slots = [0]*numSlots
-            numCollision = 0
-            for tag in range(numTags):
-                slot = rand(numSlots-1)
-                slots[slot] = slots[slot] + 1
-            for slot in slots:
-                if(slot == 0):
-                    result.numEmpty = result.numEmpty + 1;
-                elif(slot == 1):
-                    numTags = numTags - 1;
-                else:
-                    numCollision = numCollision + 1
-            result.numCollision = result.numCollision + numCollision
-            numSlots = func(numCollision)
-        results.append(result)
+    	resultTotal = Result(numTags)
+    	for i in range(1000):
+	        numSlots = 64
+	        result = Result(numTags)
+	        numTagsCount = numTags
+	        while(numTagsCount != 0):
+	            slots = [0]*numSlots
+	            numCollision = 0
+	            for tag in range(numTagsCount):
+	                slot = rand(numSlots-1)
+	                slots[slot] = slots[slot] + 1
+	            for slot in slots:
+	                if(slot == 0):
+	                    result.numEmpty = result.numEmpty + 1;
+	                elif(slot == 1):
+	                    numTagsCount = numTagsCount - 1;
+	                else:
+	                    numCollision = numCollision + 1
+	            result.numCollision = result.numCollision + numCollision
+	            numSlots = func(numCollision)
+	        resultTotal.add(result)
+        results.append(resultTotal/1000)
     return results
 
 def lowerBound(x):
     return x*2
 
-def volt(x):
-    return x
+def eomLee(x):
+    return x*2 #TODO
 
 results = aloha(lowerBound)
-results = aloha(volt)
+#results = aloha(eomLee)
 
 for result in results:
     print (result)
